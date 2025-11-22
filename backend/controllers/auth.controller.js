7374c0fdac7922f75f377cebb22fe721c5d7970a
Sabin Dhakal <137175074+mychoise@users.noreply.github.com>
2025-11-22 09:49:11 +0400

    Add secure flag to cookie settings

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -42,6 +42,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true,
+      secure:true,
       sameSite: "None",
     });
 
@@ -102,6 +103,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true,
+      secure:true,
       sameSite: "None",
     });
 
